a72afe8f778ebd808674557e2ef363d58debc60d
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2022-12-15 16:21:34 +0200

    Changed project structure to app directory instead of pages.

---
+++ app/head.js
@@ -0,0 +1,13 @@
+export default function Head() {
+ return (
+  <>
+   <title>Plugyn Store | Safe & Cheap Metin2 Yang</title>
+
+   <meta
+    name='description'
+    content='Welcome to the plugyn yang store! Here you can get some of the cheapest yang on almost all private and official Metin2 servers. Contact us today for a custom offer!'
+   />
+   <link rel='icon' href='/favicon.ico' />
+  </>
+ );
+}
