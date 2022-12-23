c0f0267d3053b221119526250261ff3b714f99fe
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2022-12-23 22:33:05 +0200

    Server side direct fetch from db instead of api call.

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,28 +1,11 @@
 import ServersList from "components/store/ServersList";
+import ServerModel from "models/Server";
+import { connect } from "utils/connection";
 import { Server } from "utils/types";
 
 async function getServers() {
-  console.log("env?", process.env.API_URL);
-  console.log("url?", process.env.VERCEL_URL);
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.API_URL;
-  console.log("baseUrl?", baseUrl);
-
-  const res = await fetch(`${baseUrl}/api/servers`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  const data = await res.json();
-  console.log(data);
-  return data;
+  await connect(); // connect to database
+  return (await ServerModel.find({})) as Server[];
 }
 
 export default async function Page() {
