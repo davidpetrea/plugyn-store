1353cc36389538b9856b402609057d8836982214
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2022-12-23 16:58:32 +0200

    Changed fetch url for getServers endpoint.

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,19 +1,19 @@
-import ServersList from 'components/store/ServersList';
+import ServersList from "components/store/ServersList";
 
 async function getServers() {
- const res = await fetch('http://localhost:3000/api/servers');
+  const res = await fetch("/api/servers");
 
- if (!res.ok) {
-  // This will activate the closest `error.js` Error Boundary
-  throw new Error('Failed to fetch data');
- }
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
- return res.json();
+  return res.json();
 }
 
 const Store = async () => {
- const servers = await getServers();
- return <ServersList servers={servers} />;
+  const servers = await getServers();
+  return <ServersList servers={servers} />;
 };
 
 export default Store;
