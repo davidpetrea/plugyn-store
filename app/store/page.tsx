2c58e025e87160ad7de7a4bcb188c5c5ce760296
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2022-12-24 10:28:21 +0200

    Adde revalidate for server info cache.

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -2,13 +2,14 @@ import ServersList from "components/store/ServersList";
 import ServerModel from "models/Server";
 import { connect } from "utils/connection";
 import { Server } from "utils/types";
+import { cache } from "react";
 
-async function getServers() {
+const getServers = cache(async () => {
   await connect(); // connect to database
   const res = (await ServerModel.find({}).lean()) as Server[];
 
   return res;
-}
+});
 
 export default async function Page() {
   const servers = (await getServers()).map((elem: Server) => {
@@ -18,3 +19,5 @@ export default async function Page() {
   if (!servers.length) return null;
   return <ServersList servers={servers as Server[]} />;
 }
+
+export const revalidate = 60; // revalidate this page every 60 seconds
